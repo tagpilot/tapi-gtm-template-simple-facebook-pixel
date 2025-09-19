4917b1eec53307d5ae2ab3086ded6bf30ce82f27
Michał Frankiewicz <michal@frankiewicz.me>
2025-09-19 12:34:28 +0200

    update template.js

--- template.js
+++ template.js
@@ -13,17 +13,18 @@ const log = require('logToConsole');
 const templateStorage = require('templateStorage');
 
 const standardEventNames = {
-  add_payment_info: 'AddPaymentInfo',
-  add_to_cart: 'AddToCart',
-  add_to_wishlist: 'AddToWishlist',
-  signup: 'CompleteRegistration',
-  generate_lead: 'Lead',
-  begin_checkout: 'InitiateCheckout',
-  'gtm.dom': 'PageView',
-  'gtm.historyChange': 'PageView',
-  purchase: 'Purchase',
-  search: 'Search',
-  view_item: 'ViewContent'
+    add_payment_info: 'AddPaymentInfo',
+    add_to_cart: 'AddToCart',
+    add_to_wishlist: 'AddToWishlist',
+    signup: 'CompleteRegistration',
+    generate_lead: 'Lead',
+    begin_checkout: 'InitiateCheckout',
+    'gtm.dom': 'PageView',
+    'gtm.js': 'PageView',
+    'gtm.historyChange': 'PageView',
+    purchase: 'Purchase',
+    search: 'Search',
+    view_item: 'ViewContent'
 };
 const ecommerce = copyFromDataLayer('ecommerce', 1);
 const eventName = copyFromDataLayer('event', 1);
@@ -35,29 +36,29 @@ const mappedEventName = standardEventNames[eventName];
 // (if the FB SDK hasn't loaded yet), or fbq.callMethod()
 // if the SDK has loaded.
 const getFbq = () => {
-  // Return the existing 'fbq' global method if available
-  let fbq = copyFromWindow('fbq');
-  if (fbq) {
-    return fbq;
-  }
-
-  // Initialize the 'fbq' global method to either use
-  // fbq.callMethod or fbq.queue)
-  setInWindow('fbq', function() {
-    const callMethod = copyFromWindow('fbq.callMethod.apply');
-    if (callMethod) {
-      callInWindow('fbq.callMethod.apply', null, arguments);
-    } else {
-      callInWindow('fbq.queue.push', arguments);
+    // Return the existing 'fbq' global method if available
+    let fbq = copyFromWindow('fbq');
+    if (fbq) {
+        return fbq;
     }
-  });
-  aliasInWindow('_fbq', 'fbq');
 
-  // Create the fbq.queue
-  createQueue('fbq.queue');
+    // Initialize the 'fbq' global method to either use
+    // fbq.callMethod or fbq.queue)
+    setInWindow('fbq', function() {
+        const callMethod = copyFromWindow('fbq.callMethod.apply');
+        if (callMethod) {
+            callInWindow('fbq.callMethod.apply', null, arguments);
+        } else {
+            callInWindow('fbq.queue.push', arguments);
+        }
+    });
+    aliasInWindow('_fbq', 'fbq');
 
-  // Return the global 'fbq' method, created above
-  return copyFromWindow('fbq');
+    // Create the fbq.queue
+    createQueue('fbq.queue');
+
+    // Return the global 'fbq' method, created above
+    return copyFromWindow('fbq');
 };
 
 log('mappedEventName', mappedEventName);
@@ -66,24 +67,24 @@ const params = {};
 const userData = {};
 
 if (data.userData && data.userData.email && data.userData.phone_number) {
-  userData.em = data.userData.email;
-  userData.ph = data.userData.phone_number;
+    userData.em = data.userData.email;
+    userData.ph = data.userData.phone_number;
 }
 
 if (!eventId && mappedEventName === 'Purchase') {
-  eventId = ecommerce.transaction_id;
+    eventId = ecommerce.transaction_id;
 }
 
 if (ecommerce) {
-  params.content_type = 'product';
-  params.value = (ecommerce.value || 0);
-  params.currency = ecommerce.currency;
-  params.contents = (ecommerce.items || []).map(function(item) {
-    return {
-      id: item.item_id,
-      quantity: item.quantity || 1,
-    };
-  });
+    params.content_type = 'product';
+    params.value = (ecommerce.value || 0);
+    params.currency = ecommerce.currency;
+    params.contents = (ecommerce.items || []).map(function(item) {
+        return {
+            id: item.item_id,
+            quantity: item.quantity || 1,
+        };
+    });
 }
 
 log('params', params, userData, templateStorage.getItem(data.pixelId), eventId);
@@ -91,20 +92,20 @@ log('params', params, userData, templateStorage.getItem(data.pixelId), eventId);
 const fbq = getFbq();
 
 if (templateStorage.getItem(data.pixelId) === null || (templateStorage.getItem(data.pixelId) === 1 && (userData.em || userData.ph))) {
-  // Initialize pixel and store in global array
-  log('FB INIT', userData);
-  fbq('init', data.pixelId, userData);
+    // Initialize pixel and store in global array
+    log('FB INIT', userData);
+    fbq('init', data.pixelId, userData);
 
-  templateStorage.setItem(data.pixelId, (templateStorage.getItem(data.pixelId) || 0) + 1);
-  // Call the fbq() method with the parameters defined earlier
+    templateStorage.setItem(data.pixelId, (templateStorage.getItem(data.pixelId) || 0) + 1);
+    // Call the fbq() method with the parameters defined earlier
 }
 
 if (typeof mappedEventName !== 'undefined') {
-  if (eventId) {
-    fbq('track', mappedEventName, params, {eventID: eventId});
-  } else {
-    fbq('track', mappedEventName, params);
-  }
+    if (eventId) {
+        fbq('track', mappedEventName, params, {eventID: eventId});
+    } else {
+        fbq('track', mappedEventName, params);
+    }
 }
 
 injectScript('https://connect.facebook.net/en_US/fbevents.js', data.gtmOnSuccess, data.gtmOnFailure, 'fbPixel');
